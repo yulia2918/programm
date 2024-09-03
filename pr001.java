e73fd4a33c6c7e659af592e1946bc2409b59d3b4
yulia2918 <yuliapolozun@gmail.com>
2024-09-03 14:11:59 +0700

    добавлены перемещение и примеры деления

--- pr001.java
+++ pr001.java
@@ -1,6 +1,13 @@
 class pr001 {
 	public static void main(String args[]) {
-		System.out.println("Первая программа на Java");
+		System.out.println("Первая программа на Java");//Выводим строку на консоль
+		int Var1 = 1024;
+		int var2;
+		//System.out.println(var2); Ошибка: переменной не присвоено значение
+		var2 = 411;
+		System.out.println(Var1/2);
+		System.out.println("результат целочисленного деления: " + var2/2);
+		 System.out.println("результат деления приведением типа данных " + (double)var2/2);
 
 	}
 
