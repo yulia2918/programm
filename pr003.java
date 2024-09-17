3dbade7ec83f79399f86aa4013ccb20d26d5609e
yulia2918 <yuliapolozun@gmail.com>
2024-09-17 13:34:15 +0700

    Добавлены программа по чтению с клавиатуры

---
+++ pr003.java
@@ -0,0 +1,11 @@
+class pr003 {
+	public static void main(String args[]) throws java.io.IOException {
+	
+		//Чтение с клавиатуры
+		char ch;
+		System.out.print("нажмите клавишу на клавиатуре и ENTER ");
+		ch = (char) System.in.read();
+		System.out.println("Вы нажали клавишу: " + ch);	
+	}
+}
+
