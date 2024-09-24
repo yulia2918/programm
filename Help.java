23374771d2998cf40674bf72e2fbedcb173ba9a6
yulia2918 <yuliapolozun@gmail.com>
2024-09-24 15:08:31 +0700

    Добавлено описание циклов, изменено поведение программы Добавили обработку выбора пользователя: в случае ошибочного выбора показывается заново содержимое меню

--- Help.java
+++ Help.java
@@ -1,12 +1,22 @@
 class Help {
 	public static void main(String args[]) throws java.io.IOException {
 		char choice;
-		System.out.println("Справка по операторам: ");
-		System.out.println("1. if");
-		System.out.println("2. switch");
-		System.out.println("Выберите номер раздела: ");
+		char ignore;
+		do {
+			
+			System.out.println("Справка по операторам: ");
+			System.out.println("1. if");
+			System.out.println("2. switch");
+			System.out.println("3. for");
+			System.out.println("4. while");
+			System.out.println("5. do-while");
+			System.out.println("Выберите номер раздела: ");
 
-		choice = (char) System.in.read();
+			choice = (char) System.in.read();
+			do {
+		       	ignore = (char) System.in.read();
+		} while (ignore != '\n');
+		} while (choice <'1'| choice > '5');
 
 		System.out.println("\n");
 		switch(choice) {
@@ -24,8 +34,21 @@ class Help {
 				System.out.println(" . . . ");
 				System.out.println("} ");
 				break;
-			default:
-				System.out.println("Неверный выбор раздела");
+			case '3':
+				System.out.println("Цикл for:\n");
+				System.out.println("for(инициализация; условие; итерация)");
+				System.out.println("  оператор;");
+				break;
+			case '4':
+				System.out.println("Цикл while:\n");
+				System.out.println("while (условие) оператор");
+				break;
+			case '5':
+				System.out.println("Цикл do-while: \n");
+				System.out.println("do {");
+				System.out.println("  оператор;");
+				System.out.println("} while (условие)");
+				break;
 		}
 	}
 
