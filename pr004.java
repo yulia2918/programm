f903b3aa161e4a22cc92f43725f11d8ce667298d
yulia2918 <yuliapolozun@gmail.com>
2024-10-01 15:28:14 +0700

    Добавили метод вычисления расстоние

--- pr004.java
+++ pr004.java
@@ -2,6 +2,10 @@ class Avto {
 	int pass; //количество пассажиров
 	int v; // объём топливного бака
 	double rash; //расход топлива (л) на 100 км
+	
+	void rasst() {
+		System.out.println("Расстояние на полном баке:+"+ ((int)(v/rash*100)));
+	}
 }
 class pr004 {
 	public static void main (String args[]) {
@@ -22,14 +26,19 @@ class pr004 {
 		porshe.v = 100;
 		porshe.rash = 14.0;
 
-
+		//lada = porshe;
 
 		//рассчёт расстояния, которое lada проедет на полном баке
 		
-		rasst = (int)( lada.v/lada.rash*100);
-		rasst2 = (int)(porshe.v/porshe.rash*100);
+		//rasst = (int)( lada.v/lada.rash*100);
+		//rasst2 = (int)(porshe.v/porshe.rash*100);
 
-		System.out.println("Lada проедет "+ rasst + " км. на полном баке ");
-		System.out.println("Porshe проедет "+rasst2+" км. на полном баке ");
+		System.out.print("Для Lada: ");
+		lada.rasst();
+
+		System.out.print("Для Porshe: ");
+		porshe.rasst();
+
+
 	}
 }
