3d95d08f99283aa695588531faf2ebcf1effedd5
yulia2918 <yuliapolozun@gmail.com>
2024-10-01 14:33:04 +0700

    добавили разделы break and continue  в справочную систему

--- Help.java
+++ Help.java
@@ -2,21 +2,26 @@ class Help {
 	public static void main(String args[]) throws java.io.IOException {
 		char choice;
 		char ignore;
-		do {
-			
+		for (;;) {
+		  do {
+		
 			System.out.println("Справка по операторам: ");
 			System.out.println("1. if");
 			System.out.println("2. switch");
 			System.out.println("3. for");
 			System.out.println("4. while");
 			System.out.println("5. do-while");
-			System.out.println("Выберите номер раздела: ");
+			System.out.println("6. break");
+			System.out.println("7. continue");
+			System.out.println("Выберите номер раздела (для завернения программы нажмите q): ");
 
 			choice = (char) System.in.read();
 			do {
-		       	ignore = (char) System.in.read();
-		} while (ignore != '\n');
-		} while (choice <'1'| choice > '5');
+		       		ignore = (char) System.in.read();
+				} while (ignore != '\n');
+			} while (choice <'1'| choice > '7'& choice !='q');
+
+		if (choice =='q') break;
 
 		System.out.println("\n");
 		switch(choice) {
@@ -49,7 +54,18 @@ class Help {
 				System.out.println("  оператор;");
 				System.out.println("} while (условие)");
 				break;
+			case '6':
+				System.out.println("Оператор break\n");
+				System.out.println("do { ");
+				System.out.println("break; или break метка; }");
+				break;
+			case '7':
+				System.out.println("Оператор continue\n");
+				System.out.println(" continue; или continue метка;");
+			}
+
 		}
+
 	}
 
 }
