c6aae45f9d755c5f3cb92a38e4ecd8daa80ebdde
yulia2918 <yuliapolozun@gmail.com>
2024-10-01 15:04:56 +0700

    Добавили новый экземпляр класса (Porshe)

--- pr004.java
+++ pr004.java
@@ -9,19 +9,27 @@ class pr004 {
 		//создание экземпляра класса
 
 		Avto lada = new Avto();
-		int rasst;
+		Avto porshe = new Avto();
+		int rasst, rasst2;
 
 		// присваивание значение внутренним переменным экземпляра класса
 
-		lada. pass = 4;
+		lada.pass = 4;
 		lada.v = 40;
-		lada.rash = 9.5;
+		lada.rash = 5.5;
+
+		porshe.pass = 1 ;
+		porshe.v = 100;
+		porshe.rash = 14.0;
+
+
 
 		//рассчёт расстояния, которое lada проедет на полном баке
 		
 		rasst = (int)( lada.v/lada.rash*100);
+		rasst2 = (int)(porshe.v/porshe.rash*100);
 
 		System.out.println("Lada проедет "+ rasst + " км. на полном баке ");
-
+		System.out.println("Porshe проедет "+rasst2+" км. на полном баке ");
 	}
 }
