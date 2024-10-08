fee11f6820669af691f9f56de2823c2ae8c6be0d
yulia2918 <yuliapolozun@gmail.com>
2024-10-08 14:37:00 +0700

    Программ Help в стиле ООП

--- Help.java
+++ Help.java
@@ -1,30 +1,6 @@
 class Help {
-	public static void main(String args[]) throws java.io.IOException {
-		char choice;
-		char ignore;
-		for (;;) {
-		  do {
-		
-			System.out.println("Справка по операторам: ");
-			System.out.println("1. if");
-			System.out.println("2. switch");
-			System.out.println("3. for");
-			System.out.println("4. while");
-			System.out.println("5. do-while");
-			System.out.println("6. break");
-			System.out.println("7. continue");
-			System.out.println("Выберите номер раздела (для завернения программы нажмите q): ");
-
-			choice = (char) System.in.read();
-			do {
-		       		ignore = (char) System.in.read();
-				} while (ignore != '\n');
-			} while (choice <'1'| choice > '7'& choice !='q');
-
-		if (choice =='q') break;
-
-		System.out.println("\n");
-		switch(choice) {
+	void helpOn (int what) {
+		switch(what) {
 			case '1' :
 				System.out.println("опертатор if\n");
 				System.out.println("if (условие) оператор");
@@ -60,13 +36,46 @@ class Help {
 				System.out.println("break; или break метка; }");
 				break;
 			case '7':
-				System.out.println("Оператор continue\n");
+				System.out.println("оператор continue\n");
 				System.out.println(" continue; или continue метка;");
+				break;
 			}
-
+			System.out.println();
 		}
-
-	}
-
+		void showMenu() {
+			System.out.println("Справка по операторам: ");
+			System.out.println("1. if ");
+			System.out.println("2. switch ");
+			System.out.println("3. for ");
+			System.out.println("4. while ");
+			System.out.println("5. do-while ");
+			System.out.println("6. break ");
+			System.out.println("7. continue ");
+			System.out.println("Выберите раздел (для завершения нажмите q): ");
+		}
+		boolean isValid(int ch) {
+			if (ch < '1'| ch > '7'& ch != 'q')
+				return false;
+			else
+				return true;
+		}
 }
 
+class HelpClassDemo {
+	public static void main (String args[]) throws java.io.IOException {
+		char choice, ignore;
+		Help hlpobj = new Help();
+		for(;;) {
+			do {
+				hlpobj.showMenu();
+				choice = (char) System.in.read();
+				do {
+					ignore = (char) System.in.read();
+				} while (ignore != '\n');
+			} while (!hlpobj.isValid(choice));
+		if(choice == 'q') break;
+		System.out.println('\n');
+		hlpobj.helpOn(choice);
+		}
+	}
+}
