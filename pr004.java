566f0da81fce424deb8210b9c2bc0280ba59eb16
yulia2918 <yuliapolozun@gmail.com>
2024-10-08 14:53:49 +0700

    Использование конструктоpа в программе pr004

--- pr004.java
+++ pr004.java
@@ -3,6 +3,11 @@ class Avto {
 	int v; // объём топливного бака
 	double rash; //расход топлива (л) на 100 км
 	
+	Avto(int p, int V, double r) {
+		pass = p;
+		v = V;
+		rash = r;
+	}
 	void rasst() {
 		System.out.println("Расстояние на полном баке:+"+ ((int)(v/rash*100)));
 	}
@@ -21,24 +26,25 @@ class pr004 {
 	
 		//создание экземпляра класса
 
-		Avto lada = new Avto();
-		Avto porshe = new Avto();
+		Avto lada = new Avto(4, 40, 5.5);
+		Avto porshe = new Avto(1, 100, 14.0);
 		int rasst, rasst2;
 
 		// присваивание значение внутренним переменным экземпляра класса
 
-		lada.pass = 4;
+		/*lada.pass = 4;
 		lada.v = 40;
 		lada.rash = 5.5;
 
 		porshe.pass = 1 ;
 		porshe.v = 100;
-		porshe.rash = 14.0;
+		porshe.rash = 14.0;*/
 
 		System.out.println("Для расстояния в 10 км. нужно "+ lada.fuelNeeded(10) + " литров бензина");
 		
-		rasst = 10;
-		System.out.println("Для расстояния в 10 км. нужно "+ lada.fuelNeeded(rasst) + " литров бензина");
+	
+		//rasst = 10;
+		//System.out.println("Для расстояния в 10 км. нужно "+ lada.fuelNeeded(rasst) + " литров бензина");
 		//lada = porshe;
 
 		//рассчёт расстояния, которое lada проедет на полном баке
@@ -54,9 +60,5 @@ class pr004 {
 		System.out.print("Для Porshe: ");
 		porshe.rasst();
 		System.out.println("Результат rasst2(): " + porshe.rasst2());
-
-
-
-
 	}
 }
