d2bc4d08adbe40e1cdf19d318f44cdf1035ebaad
yulia2918 <yuliapolozun@gmail.com>
2024-11-19 13:37:49 +0700

    добавили подкласс грузовики в программу

--- pr004.java
+++ pr004.java
@@ -1,7 +1,7 @@
 class Avto {
-	int pass; //количество пассажиров
-	int v; // объём топливного бака
-	double rash; //расход топлива (л) на 100 км
+	private int pass; //количество пассажиров
+	private int v; // объём топливного бака
+	private double rash; //расход топлива (л) на 100 км
 	
 	Avto(int p, int V, double r) {
 		pass = p;
@@ -19,8 +19,52 @@ class Avto {
 	double fuelNeeded (int km) {
 		return ((double)km/100)*rash;
 	}
+
+	//методы доступа
 	
+	int getPass() {
+		return pass;
+	}
+
+	void setPass(int p) {
+		pass = p;
+	}
+	
+	int getV() {
+		return v;
+	}
+
+	void setV(int V) {
+		v = V;
+	}
+
+	double getRash() {
+		return rash;
+	}
+
+	void setRash(double r) {
+		rash = r;
+	}
 }
+
+class Truck extends Avto {
+	private int cargocap; //грузоподъёмность
+	
+	//конструкторы
+	Truck(int p, int V, double r, int c) {
+		super(p, V, r);
+		cargocap = c;
+	}
+
+	int getCargo() {
+		return cargocap;
+	}
+
+	void putCargo(int c) {
+		cargocap = c;
+	}
+}
+
 class pr004 {
 	public static void main (String args[]) {
 	
@@ -28,6 +72,8 @@ class pr004 {
 
 		Avto lada = new Avto(4, 40, 5.5);
 		Avto porshe = new Avto(1, 100, 14.0);
+		Truck gazelnext = new Truck(2, 100, 25.5, 3500);
+		Truck ural = new Truck(3, 200, 35.1, 5000);
 		int rasst, rasst2;
 
 		// присваивание значение внутренним переменным экземпляра класса
@@ -40,7 +86,12 @@ class pr004 {
 		porshe.v = 100;
 		porshe.rash = 14.0;*/
 
-		System.out.println("Для расстояния в 10 км. нужно "+ lada.fuelNeeded(10) + " литров бензина");
+		System.out.println("Для расстояния в 10 км. ладе нужно "+ lada.fuelNeeded(10) + " литров бензина");
+		System.out.println("Для расстояния в 10 км. уралу нужно "+ ural.fuelNeeded(10) + " литров бензина");
+		System.out.println("Урал перевезёт " + ural.getCargo() + " кг. грузов");
+		System.out.println("Для расстояния в 10 км. газели нужно "+ gazelnext.fuelNeeded(10) + " литров бензина");
+		rasst = 10;
+		System.out.println("Для расстояния в 10 км. порше нужно "+ porshe.fuelNeeded(rasst) + " литров бензина");
 		
 	
 		//rasst = 10;
