9943bd208c24e2629242839242ac7daac6900851
yulia2918 <yuliapolozun@gmail.com>
2024-12-10 11:27:27 +0700

    Демонстрация интерфейса Series и его реализации ByTwos

---
+++ SeriesDemo.java
@@ -0,0 +1,19 @@
+class SeriesDemo {
+	public static void main(String[] args) {
+		ByTwos ob = new ByTwos();
+		
+		for (int i = 0; i < 5; i++)
+			System.out.println("Следующее число в последовательности: " + ob.getNext());
+		System.out.println("\nСброс последовательности к стартовому значению");
+		ob.reset();
+		
+		for (int i = 0; i < 5; i++)
+			System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("\nСброс последовательности к стартовому значению");
+		ob.reset();
+
+		ob.setStart(100);
+		for (int i = 0; i < 5; i++)
+                        System.out.println("Следующее значение в последовательности: " + ob.getNext());
+	}
+}
--- bookpackext/pr011.java
+++ bookpackext/pr011.java
@@ -1,13 +1,20 @@
 package bookpackext;
 
+//импорт класса из пакета bookpack
+
+import bookpack.book;
+
+//импорт всех классов из пакета bookpack
+//import bookpack
+
 class pr011 {
 	public static void main(String[] args) {
-                bookpack.book[] books = new bookpack.book[5];
-                books[0] = new bookpack.book("Иллиада", "Гомер", 1980);
-                books[1] = new bookpack.book("Унесённые ветром", "Митчел", 2000);
-                books[2] = new bookpack.book("Наедине с собой", "Аврелий", 1950);
-                books[3] = new bookpack.book("Собор Парижской богоматери", "Гюго", 1970);
-                books[4] = new bookpack.book("Преступление и наказание", "Достоевский", 1950);
+                book[] books = new book[5];
+                books[0] = new book("Иллиада", "Гомер", 1980);
+                books[1] = new book("Унесённые ветром", "Митчел", 2000);
+                books[2] = new book("Наедине с собой", "Аврелий", 1950);
+                books[3] = new book("Собор Парижской богоматери", "Гюго", 1970);
+                books[4] = new book("Преступление и наказание", "Достоевский", 1950);
 
                 for (int i=0; i< books.length; i++)
                         books[i].Show();
