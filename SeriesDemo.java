af71e4be1ef017a56e87f79c9bd5ea8b3aea870d
yulia2918 <yuliapolozun@gmail.com>
2024-12-10 11:40:38 +0700

    Добавили метод получения предыдущего значения

--- SeriesDemo.java
+++ SeriesDemo.java
@@ -4,16 +4,19 @@ class SeriesDemo {
 		
 		for (int i = 0; i < 5; i++)
 			System.out.println("Следующее число в последовательности: " + ob.getNext());
+		System.out.println("Последнее предыдущее зачение: " + ob.getPrevious());
 		System.out.println("\nСброс последовательности к стартовому значению");
 		ob.reset();
 		
 		for (int i = 0; i < 5; i++)
 			System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("Последнее предыдущее зачение: " + ob.getPrevious());
 		System.out.println("\nСброс последовательности к стартовому значению");
 		ob.reset();
 
 		ob.setStart(100);
 		for (int i = 0; i < 5; i++)
                         System.out.println("Следующее значение в последовательности: " + ob.getNext());
+		System.out.println("Последнее предыдущее зачение: " + ob.getPrevious());
 	}
 }
